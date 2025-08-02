0063a4975596036f09a7fd2993fe39132db29995
seltan-g8 <snbda3010@gmail.com>
2025-08-02 13:15:43 +0100

    removed phone from use schema

--- models/user.js
+++ models/user.js
@@ -3,7 +3,6 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
     name: { type: String, required: true },
-    phone: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 },
